801fd34ae1cb910676a022d4884ea0c0a54a3c0c
Peter-Sciberras <sciberrasp@yahoo.com.au>
2022-12-18 11:50:18 +1100

    done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+//variable multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+//variable random
+let random = Math.ceil(Math.random() * 100);
+
+//variable mod
+let num3 = 56;
+let num4 = 13;
+let mod = num3 % num4;
+
+//variable max
+let numbers = [7, 9, 15, 20];
+let max = Math.max(...numbers);
